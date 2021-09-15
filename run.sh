92cec3f79b25ede1fee72bca98e6865e86ee0cac
Yue Ying <hardyying@gmail.com>
2021-09-15 09:30:18 +0200

    run script on betzy to speed up

--- run.sh
+++ run.sh
@@ -1,16 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash
 #SBATCH --account=nn2993k
-#SBATCH --job-name=vortex
-#SBATCH --time=0-06:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
+#SBATCH --job-name=msa_test
+#SBATCH --time=0-01:00:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=128
+#SBATCH --qos=devel
+#SBATCH --output=log/slurm-%j.out
 
-source ~/.bashrc
+source $HOME/.bashrc
 cd /cluster/home/yingyue/code/rankine
 
-for L in 4 6; do
-    for ROI in 8 12 16 20 24 32 40 48 64; do
-        python run_full.py 2 $L $ROI &
-    done
+nt=$SLURM_NTASKS
+ppn=$SLURM_NTASKS_PER_NODE
+
+##single_wind_obs exps
+t=0
+for real in `seq 1 128`; do
+    offset_node=`echo $t / $ppn |bc`
+    echo $real
+    #srun -N 1 -n 1 -r $offset_node python run_single_wind_obs.py $real &
+    srun -N 1 -n 1 -r $offset_node python run_position_obs.py $real &
+    t=$((t+1))
+    if [ $t == $nt ]; then
+        t=0
+        wait
+    fi
 done
 wait
+kill -HUP $PPID
