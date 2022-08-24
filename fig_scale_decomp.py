1cd75ac0a7da5d5b06883a0f2ef13a2c395de1bc
Yue Ying <hardyying@gmail.com>
2022-08-24 16:32:59 +0200

    add obs SCs in scale decomp plot

--- fig_scale_decomp.py
+++ fig_scale_decomp.py
@@ -5,43 +5,90 @@ from data_assimilation import *
 from multiscale import *
 from config import *
 
-Vbg = 4
-np.random.seed(100)
-Xout = gen_random_flow(ni, nj, nv, dx, Vbg, -3) + gen_vortex(ni, nj, nv, Vmax, Rmw, 0)
-ii, jj = np.mgrid[0:ni, 0:nj]
-iout = (ii-ni/2)*dx/1000
-jout = (jj-nj/2)*dx/1000
+def set_axes(ax):
+    ax.set_aspect('equal', 'box')
+    ax.set_xlim(14, 114)
+    ax.set_ylim(14, 114)
+    ax.set_xticks(np.arange(20, 109, 22))
+    ax.set_yticks(np.arange(20, 109, 22))
+    ax.set_xticklabels(np.arange(-400, 401, 200))
+    ax.set_yticklabels(np.arange(-400, 401, 200))
+    ax.tick_params(labelsize=12)
 
-ns = 3
+np.random.seed(100)
+
+Xt = gen_random_flow(ni, nj, nv, dx, Vbg, -3) + gen_vortex(ni, nj, nv, Vmax, Rmw, 0)
+ii, jj = np.mgrid[0:ni, 0:nj]
+
+ns = 4 ##>=4
 Xsc = np.zeros((ni, nj, nv, ns))
 
 plt.switch_backend('Agg')
-plt.figure(figsize=(14,9))
+plt.figure(figsize=(18, 18))
 
-ax = plt.subplot(222)
-c = ax.contourf(iout, jout, Xout[:, :, 0], np.arange(-40, 41, 4), cmap='bwr')
-ax.set_aspect('equal')
-ax.tick_params(labelsize=13)
-cb = plt.colorbar(c)
-cb.ax.tick_params(labelsize=12)
 
-ax1 = plt.subplot(221)
-wn, pwr = pwr_spec(Xout)
-ax1.loglog(wn, pwr, color='black', linewidth=3, label='total')
+# c = ax.contourf(ii, jj, Xt[:, :, 0], np.arange(-40, 41, 4), cmap='bwr')
+# c = ax.contourf(ii, jj, get_wspd(Xt), np.arange(0, 41, 2), cmap='bwr')
+# ax.set_aspect('equal')
+# ax.tick_params(labelsize=13)
+# cb = plt.colorbar(c)
+# cb.ax.tick_params(labelsize=12)
 
+###power spectrum panel
+ax = plt.subplot(4, ns, 1)
+wn, pwr = pwr_spec(Xt)
+ax.loglog(wn, pwr, color='black', linewidth=3, label='total')
 for s in range(ns):
-    Xs = get_scale(Xout, get_krange(ns), s)
+    Xs = get_scale(Xt, get_krange(ns), s)
     wn, pwr = pwr_spec(Xs)
-    ax = plt.subplot(2,3,s+4)
-    ax.tick_params(labelsize=13)
-    c = ax.contourf(iout, jout, Xs[:, :, 0], np.arange(-20, 21, 2), cmap='bwr')
-    cb = plt.colorbar(c)
-    cb.ax.tick_params(labelsize=12)
-    ax1.loglog(wn, pwr, linewidth=2, label='s={}'.format(s+1))
-ax1.set_xlim([1, 40])
-ax1.set_ylim([1e-3, 1e2])
-ax1.legend()
-ax1.tick_params(labelsize=12)
+    ax.loglog(wn, pwr, linewidth=2, label='s={}'.format(s+1)) #, color=cmap[s][0:3])
+ax.set_xlim([1, 40])
+ax.set_ylim([1e-3, 1e2])
+ax.legend(fontsize=12)
+ax.tick_params(labelsize=12)
 
+##full state
+ax = plt.subplot(4, ns, 2)
+Xout = Xt.copy()
+Xout[np.where(Xout>30)] = 30
+Xout[np.where(Xout<-30)] = -30
+c = ax.contourf(ii, jj, Xout[:, :, 0], np.arange(-30, 31, 2), cmap='bwr')
+plt.colorbar(c)
+set_axes(ax)
+for s in range(ns):
+    #state SCs
+    ax = plt.subplot(4, ns, ns+s+1)
+    Xs = get_scale(Xt, get_krange(ns), s)
+    ax.contourf(ii, jj, Xs[:, :, 0], np.arange(-30, 31, 2), cmap='bwr')
+    set_axes(ax)
+
+for network_type in (1, 2):
+    np.random.seed(101)
+    nobs, obs_range = gen_network(network_type)
+    true_center = vortex_center(Xt)
+    Yo = np.zeros((nobs*nv))
+    Ymask = np.zeros((nobs*nv))
+    Yloc = np.zeros((3, nobs*nv))
+    Xo = Xt.copy()
+    for k in range(nv):
+        Xo[:, :, k] += obs_err_std * random_field(ni, obs_err_power_law)
+    Yloc = gen_obs_loc(ni, nj, nv, nobs)
+    Yo = obs_interp2d(Xo, Yloc)
+    Ydist = get_dist(ni, nj, Yloc[0, :], Yloc[1, :], true_center[0], true_center[1])
+    Ymask[np.where(Ydist<=obs_range)] = 1
+    Yo[np.where(Ymask==0)] = 0.0
+
+    ##full obs
+    ax = plt.subplot(4, ns, network_type+2)
+    plot_obs(ax, ni, nj, nv, Yo, Ymask, Yloc)
+    set_axes(ax)
+
+    ###state SCs
+    for s in range(ns):
+        ##obs SCs
+        ax = plt.subplot(4, ns, (network_type+1)*ns+s+1)
+        Yos = obs_get_scale(ni, nj, nv, Yo, Ymask, Yloc, get_krange(ns), s)
+        plot_obs(ax, ni, nj, nv, Yos, Ymask, Yloc)
+        set_axes(ax)
 
 plt.savefig('out.pdf')
--- obs_def.py
+++ obs_def.py
@@ -3,7 +3,7 @@ import numpy as np
 ###observation network
 def gen_network(network_type):
     if network_type==1:  ##global network
-        nobs = 500
+        nobs = 1000
         obs_range = 100
     if network_type==2:  ##targeted network
         nobs = 800
@@ -12,8 +12,15 @@ def gen_network(network_type):
 
 def gen_obs_loc(ni, nj, nv, nobs):
     Yloc2 = np.zeros((2, nobs))
+    ###random location
     Yloc2[0, :] = np.random.uniform(0, ni, nobs)
     Yloc2[1, :] = np.random.uniform(0, nj, nobs)
+    ###uniform location
+    # ii, jj = np.mgrid[0:ni, 0:nj]
+    # d = np.round(ni/np.sqrt(nobs)).astype(int)
+    # Yloc2[0, :] = np.reshape(ii[::d, ::d].astype(float), nobs)
+    # Yloc2[1, :] = np.reshape(jj[::d, ::d].astype(float), nobs)
+
     Yloc = np.zeros((3, nobs*nv))
     for k in range(nv):
         Yloc[0:2, k::nv] = Yloc2
@@ -47,9 +54,22 @@ def plot_obs(ax, ni, nj, nv, Y, Ymask, Yloc):
     import matplotlib.pyplot as plt
     obsmin, obsmax = (-30, 30)
     cmap = [plt.cm.bwr(x) for x in np.linspace(0, 1, round(obsmax-obsmin)+1)]
-    subset = np.where(np.logical_and(Yloc[2, :]==0, Ymask==1))
+    subset = np.where(np.logical_and(Yloc[2, :]==0, Ymask==1))  ##show u obs in obs loc (unmasked)
     color_ind = np.maximum(np.minimum(np.round(Y[subset]-obsmin), int(round(obsmax-obsmin))), 0).astype(int)
-    ax.scatter(Yloc[0, subset], Yloc[1, subset], s=10, color=np.array(cmap)[color_ind, 0:3])
+    ax.scatter(Yloc[0, subset], Yloc[1, subset], s=20, color=np.array(cmap)[color_ind, 0:3])
+    ax.set_xlim([0, ni])
+    ax.set_ylim([0, nj])
+    return
+
+def plot_obs_wspd(ax, ni, nj, nv, Y, Ymask, Yloc):
+    import matplotlib.pyplot as plt
+    obsmin, obsmax = (0, 40)
+    cmap = [plt.cm.bwr(x) for x in np.linspace(0, 1, round(obsmax-obsmin)+1)]
+    ind1 = np.where(np.logical_and(Yloc[2, :]==0, Ymask==1))  ##u obs
+    ind2 = np.where(np.logical_and(Yloc[2, :]==1, Ymask==1))  ##v obs
+    W = np.sqrt(Y[ind1]**2 + Y[ind2]**2)  ##obs wind speed
+    color_ind = np.maximum(np.minimum(np.round(W-obsmin), int(round(obsmax-obsmin))), 0).astype(int)
+    ax.scatter(Yloc[0, ind1], Yloc[1, ind1], s=20, color=np.array(cmap)[color_ind, 0:3])
     ax.set_xlim([0, ni])
     ax.set_ylim([0, nj])
     return
